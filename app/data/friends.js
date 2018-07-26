fd50f315f48273ac70c46aac8a112e2c28b546c5
Jordan <tjordanwells@gmail.com>
2018-07-26 19:02:29 -0400

    added two more matches in db

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var users = [
     {
         name: "Dana Scully",
-        photo: "http://opticianado.com/wp-content/uploads/2016/09/01x15_youngatheart_scullyglasses03.jpg",
+        photo: "https://vignette.wikia.nocookie.net/p__/images/1/11/Scully_%28X-files%29.jpg/revision/latest?cb=20150927122300&path-prefix=protagonist",
         scores: [
             1,
             2,
@@ -29,6 +29,38 @@ var users = [
             2,
             1,
             2
+        ]
+    },
+    {
+        name: "Audrey Horne",
+        photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/06/12/04/audrey-horne-0.jpg?w968h681",
+        scores: [
+            3,
+            3,
+            3,
+            5,
+            4,
+            1,
+            1,
+            3,
+            2,
+            5
+        ]
+    },
+    {
+        name: "Fred Armisen",
+        photo: "https://media4.s-nbcnews.com/j/streams/2013/January/130108/1B5446488-130801-fred-armisen.fit-760w.jpg",
+        scores: [
+            5,
+            4,
+            5,
+            5,
+            2,
+            3,
+            4,
+            1,
+            2,
+            3
         ]
     }
 ];
